04b2b97c603395acca1a6d6b098cc0047cd7eb91
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-05-17 23:13:04 -0600

    Segundo commits se realizo solo maquetacion y algunos estilos scss

--- index.php
+++ index.php
@@ -3,7 +3,110 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>CV-Intecap</title>
   </head>
-  <body></body>
+  <body>
+    <div class="wrapper">
+      <section class="main">
+        <div class="item left">
+          <div class="profile"></div>
+          <div class="perfil">
+            <p>Frontend developer</p>
+            <p>Guatemala, Guatemala</p>
+            <p>joseisraelss@hotmail.com</p>
+          </div>
+          <hr class="division">
+          <div class="skillContainer">
+            <h3>Skills</h3>
+            <p>html</p>
+            <div class="skill">
+              <div class="colorGreen"></div>
+            </div>
+            <p>CSS</p>
+            <div class="skill">
+              <div class="colorGreen"></div>
+            </div>
+            <p>Javascript</p>
+            <div class="skill">
+              <div class="colorGreen"></div>
+            </div>
+            <p>C#</p>
+            <div class="skill">
+              <div class="colorGreen"></div>
+            </div>
+            <p>Java</p>
+            <div class="skill">
+              <div class="colorGreen"></div>
+            </div>
+          </div>
+          <div class="languages">
+            <h3>Languager</h3>
+            <p>Español</p>
+            <div class="skill">
+              <div class="colorGreen"></div>
+            </div>
+            <p>English</p>
+            <div class="skill">
+              <div class="colorGreen"></div>
+            </div>
+          </div>
+        </div>
+        <div class="item right1">
+          <h2>Work Experience</h2>
+          <h3>Front End Developer</h3>
+          <div class="right1Icon">
+            <img src="/" alt="img">
+            <p>May 2020 - <span>Current</span></p>
+          </div>
+          <div class="descriptionExperience">
+            <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+          </div>
+          <div class="right1Icon">
+            <img src="/" alt="img">
+            <p>May 2020 - <span>Current</span></p>
+          </div>
+          <div class="descriptionExperience">
+            <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+          </div>
+          <div class="right1Icon">
+            <img src="/" alt="img">
+            <p>May 2020 - <span>Current</span></p>
+          </div>
+          <div class="descriptionExperience">
+            <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+          </div>
+        </div>
+        <div class="item education">
+          <h2>Education</h2>
+          <h3>Intecap</h3>
+          <div class="educationIcon">
+            <img src="" alt="img">
+            <p>Forever</p>
+          </div>
+          <p>Web Development! All I need to know in one place</p>
+          <hr>
+          <div class="item education">
+          <h2>Education</h2>
+          <h3>Intecap</h3>
+          <div class="educationIcon">
+            <img src="" alt="img">
+            <p>Forever</p>
+          </div>
+          <p>Web Development! All I need to know in one place</p>
+          <hr>
+          <div class="item education">
+          <h2>Education</h2>
+          <h3>Intecap</h3>
+          <div class="educationIcon">
+            <img src="" alt="img">
+            <p>Forever</p>
+          </div>
+          <p>Web Development! All I need to know in one place</p>
+          <hr>
+        </div>
+      </section>
+    </div>   
+    <section class="footer">Footer</section>
+  </body>
 </html>
